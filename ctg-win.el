604d17cf54154e89486567df2fc8d8a650f52a96
Coutego <coutego@gmail.com>
2021-11-22 22:19:30 +0100

    add toggle window split

--- ctg-win.el
+++ ctg-win.el
@@ -40,6 +40,33 @@ with a call to [ctg-win/save-window-state]"
             (ht-map (lambda (k v) (format "%s" k)) ctg-win--window-configs)
             :action #'ctg-win--do-restore))
 
+(defun ctg-toggle-window-split ()
+  "Change the orientation of a two window split.
+
+If the windows are stacked horizontally, stack them vertically and viceversa.
+Based on code borrowed from a StakOverflow answer."
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter (if (= (car this-win-edges) (car (window-edges (next-window))))
+                           'split-window-horizontally
+                         'split-window-vertically)))
+         (delete-other-windows)
+         (let ((first-win (selected-window)))
+           (funcall splitter)
+           (if this-win-2nd (other-window 1))
+           (set-window-buffer (selected-window) this-win-buffer)
+           (set-window-buffer (next-window) next-win-buffer)
+           (select-window first-win)
+           (if this-win-2nd (other-window 1))))))
+
 (provide 'ctg-win)
 
 ;;; ctg-win.el ends here
